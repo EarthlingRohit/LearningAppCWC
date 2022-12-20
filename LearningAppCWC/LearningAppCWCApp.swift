7d2b8fa9a5ca13bde821cb144b81585c66a4a568
Rohit Singh <76609783+RohitSinghHQ@users.noreply.github.com>
2022-12-20 14:43:23 -0500

    Initial Commit

---
+++ LearningAppCWC/LearningAppCWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppCWCApp.swift
+//  LearningAppCWC
+//
+//  Created by Rohit Singh on 2022-12-20.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppCWCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
